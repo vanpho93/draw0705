4037e75c6a1beb4698c83ecec3da1571a7534b51
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-11 20:29:12 +0700

    b.js

--- b.js
+++ b.js
@@ -9,4 +9,22 @@ function veHinh(n, fn) {
     }
 }
 
-veHinh(5, (i, j, n) => (i + j >= n + 1));
+veHinh(5, (i, j) => i >= j);
+// veHinh(5, (i, j, n) => i + j >= n + 1);
+// veHinh(5, (i, j) => i <= j);
+// veHinh(5, (i, j, n) => {
+//     const soBuocChan = (n - 1) / 2;
+//     const trungTam = (n + 1) / 2;
+//     const soBuocDoc = Math.abs(trungTam - i);
+//     const soBuocNgang = Math.abs(trungTam - j);
+//     return soBuocNgang + soBuocDoc <= soBuocChan;
+// });
+// veHinh(9, (i, j, n) => {
+//     const soBuocChan = (n - 1) / 2;
+//     const trungTam = (n + 1) / 2;
+//     const soBuocDoc = Math.abs(trungTam - i);
+//     const soBuocNgang = Math.abs(trungTam - j);
+//     const namTrenCanh = soBuocNgang + soBuocDoc === soBuocChan;
+//     const namTrenDuongCheo = i === trungTam || j === trungTam;
+//     return namTrenCanh || namTrenDuongCheo;
+// });
